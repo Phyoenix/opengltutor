b730bcc0b5a2e942aa35cdc2effee2747909bc84
Shenghao <tangshenghao2@gmail.com>
2025-03-03 07:16:20 +0800

    ?

---
+++ build/CMakeFiles/opengltutor.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengltutor.dir/src/Game.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/Game.cpp.o.d"
+  "CMakeFiles/opengltutor.dir/src/Image.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/Image.cpp.o.d"
+  "CMakeFiles/opengltutor.dir/src/InputCtl.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/InputCtl.cpp.o.d"
+  "CMakeFiles/opengltutor.dir/src/OBJ.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/OBJ.cpp.o.d"
+  "CMakeFiles/opengltutor.dir/src/check_gl.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/check_gl.cpp.o.d"
+  "CMakeFiles/opengltutor.dir/src/main.cpp.o"
+  "CMakeFiles/opengltutor.dir/src/main.cpp.o.d"
+  "opengltutor"
+  "opengltutor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengltutor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/opengltutor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for opengltutor.
+# This may be replaced when dependencies are built.
